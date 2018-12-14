94df62b9ba1e5e75966f94408571426ac3b2083f
Ivan Baranov <3903672@gmail.com>
2018-12-14 18:16:26 +0300

    2

--- core/src/com/gmail/wizaripost/miner/WizariMiner.java
+++ core/src/com/gmail/wizaripost/miner/WizariMiner.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class WizariMiner extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -18,7 +18,7 @@ public class WizariMiner extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 50, 50);
